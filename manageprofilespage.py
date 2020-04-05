4437c1d5f0fa48fbcb51aa3ffb3c43933dabd3b0
unknown <bradleypelton@gmail.com>
2020-04-05 12:00:21 -0400

    10 change profile setting functions

--- manageprofilespage.py
+++ manageprofilespage.py
@@ -16,7 +16,7 @@ import genrepagetools
 
 user_login(secrets.bradleys_email,secrets.bradleys_password)
 
-# https://www.netflix.com/profiles/manage
+https://www.netflix.com/profiles/manage
 
 #########################################################################
 #########################################################################
@@ -42,6 +42,26 @@ def get_profile_names(driver) -> List:
     profiles = driver.find_elements_by_css_selector('ul.choose-profile > li')
     profile_names = [profile.text for profile in profiles if profile.text != "Add Profile"]
     return(profile_names)
+
+def edit_profile_is_open(driver) -> bool:
+    """returns true if the edit profile modal is open, false if else"""
+    try:
+        driver.find_element_by_css_selector('div.profile-edit-inputs > input')  # name_field 
+        return(True)
+    except NoSuchElementException:
+        return(False)
+
+def open_named_profile(driver, profile_name):
+    """ opens the profile with name: profile_name. 
+    https://www.netflix.com/ManageProfiles MUST BE OPEN """
+    #
+    assert driver.current_url == 'https://www.netflix.com/ManageProfiles', 'wrong location'
+    profiles = driver.find_elements_by_css_selector('ul.choose-profile > li')
+    for profile in profiles:
+        if profile.text == profile_name:
+            avatar_wrapper = profile.find_element_by_css_selector('div.avatar-wrapper')
+            avatar_wrapper.click()
+            break
 
 
 def create_new_profile(driver, profile_name: str, for_kids=False, for_teens=False):
@@ -74,6 +94,7 @@ def create_new_profile(driver, profile_name: str, for_kids=False, for_teens=Fals
 
 def delete_profile(driver, the_profile_name):
     """ TODO- CLEAN THIS UP. FILTHY"""
+    """ opening and closing the edit modal will be handled elsewhere"""
     all_profile_names = get_profile_names(driver)
     assert the_profile_name in all_profile_names, "Attempted to delete unknown profile"
     # refactor this, awkward and duplicate logic
@@ -98,7 +119,7 @@ def delete_profile(driver, the_profile_name):
 
 
 def get_profile_settings(driver) -> dict:
-    """ PROFILE HAS TO BE OPEN TODO- WRITE A FUCNTION THAT OPENS THIS PAGE"""
+    """ opening and closing the edit modal will be handled elsewhere"""
     """ return a dictionary of the following settings:
     name: str
     kid?: bool
@@ -146,18 +167,10 @@ def get_profile_settings(driver) -> dict:
         profile_settings['autoplay_previews'] = True
     except NoSuchElementException:
         profile_settings['autoplay_previews'] = False
-    print(profile_settings)
+    return(profile_settings)
 
 
-def change_profile_settings(
-    driver,
-    name=False,
-    kid=False,
-    current_language=False,
-    allowed_shows=False,
-    autoplay_episode=False,
-    autoplay_previews=False
-)
+def change_profile_settings(driver, **kwargs):
     """ CHANGE ONE OF THE FOLLOWING SETTINGS
             name: str
             kid?: bool
@@ -166,3 +179,107 @@ def change_profile_settings(
             autoplay_episode: bool
             autoplay_previews: bool
     """
+    """ TODO- "nice to have". a master function that changes all of the settings at the same time
+    """
+    pass
+
+def change_profile_name(driver, new_name: str):
+    """ TODO """
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_name != current_profile_settings['name'], "Name is the same. change_profile_name"
+    name_field = driver.find_element_by_css_selector('div.profile-edit-inputs > input')
+    name_field.clear()
+    name_field.send_keys(new_name)
+
+def change_kid_bool(driver, new_bool: bool):
+    """TODO"""
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_bool != current_profile_settings['kid'], 'same kid status, change_kid_bool'
+    # clicking the checkbox works for unchecking and checking
+    kids_checkbox = driver.find_element_by_css_selector('label[for="add-kids-profile"]')
+    kids_checkbox.click()
+
+def change_default_language(driver, new_language: str):
+    """ There are 26 languages, most not UTF-8 compliant. TODO- Create and display a dictionary so
+    a user of this script could choose a corresponding key to a language, then 
+    { 'english': 4, 'Español':5, 'Français',:6 } take the value and pass it into nth:child
+    """
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_language != current_profile_settings['language'], "same language, change_default_lan"
+    if new_language.lower() == 'english':
+        language_dropdown = driver.find_element_by_css_selector('div.nfDropDown.theme-lakira > div')
+        language_dropdown.click()
+        english_choice = driver.find_element_by_css_selector(
+            'div.sub-menu.theme-lakira > ul > li:nth-of-type(4)'
+        )
+    if new_language.lower() == 'español':
+        language_dropdown = driver.find_element_by_css_selector('div.nfDropDown.theme-lakira > div')
+        language_dropdown.click()
+        spanish_choice = driver.find_element_by_css_selector(
+        'div.sub-menu.theme-lakira > ul > li:nth-of-type(5)'
+        )
+        spanish_choice.click()
+
+def change_allowed_shows(driver, new_allowed_shows):
+    """TODO- BUG- in here somewhere. looks good 99% of the time"""
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_allowed_shows != current_profile_settings['allowed_shows']
+    maturity_dropdown = driver.find_element_by_css_selector('div[data-uia="maturity-drop-down"]')
+    maturity_dropdown.click()
+    # print('gate 1')
+    #
+    if new_allowed_shows.lower() == 'for little kids only':
+        option = 1
+    #
+    elif new_allowed_shows.lower() == 'for older kids and below':
+        option = 2
+    elif new_allowed_shows.lower() == 'for teens and below':
+        option = 3
+        # NETFLI HIDES THE LAST TWO OPTIONS IF kids_checkbox is checked
+        if current_profile_settings['kid']:
+            change_kid_bool(driver,False)
+            time.sleep(3)
+    elif new_allowed_shows.lower() == 'all maturity levels':
+        option = 4
+        # NETFLI HIDES THE LAST TWO OPTIONS IF kids_checkbox is checked
+        if current_profile_settings['kid']:
+            change_kid_bool(driver,False)
+            time.sleep(3)
+    else:
+        raise TypeError("DIDNT RECOGNIZED new_allowed_shows parameter in changed_allowed_shows")
+    #
+    # print('gate 2')
+    dropdown_choice = driver.find_element_by_css_selector(
+        'div[data-uia="maturity-drop-down"] > div.sub-menu.theme-lakira > ul > li:nth-of-type(' \
+        + str(option) + ')'
+    )
+    dropdown_choice.click()
+
+# change_allowed_shows(driver, 'for teens and below')
+# change_allowed_shows(driver, 'all maturity levels')
+# change_allowed_shows(driver, 'for older kids and below')
+# change_allowed_shows(driver, 'for little kids only')
+
+def change_autoplay_episode(driver, new_bool: bool):
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_bool != current_profile_settings['autoplay_episode'], "same bool, change_autoplay_e"
+    autoplay_episode_checkbox = driver.find_element_by_css_selector(
+        'label[for="nextEpisode-profile"]'
+    )
+    autoplay_episode_checkbox.click()
+
+
+def change_autoplay_previews(driver, new_bool: bool):
+    """ opening and closing the edit modal will be handled elsewhere"""
+    current_profile_settings = get_profile_settings(driver)
+    assert new_bool != current_profile_settings['autoplay_previews'], "same bool, change_autoplay_p"
+    #
+    autoplay_previews_checkbox = driver.find_element_by_css_selector(
+    'label[for="videomerch-profile"]'
+    )
+    autoplay_previews_checkbox.click()
