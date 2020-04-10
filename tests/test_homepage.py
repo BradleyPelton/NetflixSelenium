fa43cb2fd61845cb7fa933594c4733da360edcf5
unknown <bradleypelton@gmail.com>
2020-04-10 18:43:34 -0400

    unittest syntax

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -9,13 +9,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 import pagemodels.homepage
-import tests.logintests
+import tests.test_loginpage
 
 # THE DRIVER IS HANDLED BY THE LOGINTESTS AND THEN PASSED FROM TEST OBJECT TO TEST OBJECT
 # chromedriver_path = prerefactor.secrets.chromedriver_path
 # driver = webdriver.Chrome(executable_path=chromedriver_path)
 
-login_container = tests.logintests.LoginTests()
+login_container = tests.test_loginpage.LoginTests()
 login_container.user_login_main()
 # TODO- rename this. its not a login_container
 
@@ -24,25 +24,30 @@ login_container.user_login_main()
 # of the test. A method should be created to gain access to the AUT* (e.g. using an API to login
 # and set a cookie).
 
+
 class HomePageTests(unittest.TestCase):
 
-    def __init__(self):
+    def setUp(self):
         self.driver = login_container.driver
         # this is a temporary patch for a larger problem, namely what to do with the driver
         # I need to be able to pass the driver from page object to page object 
         # Also these tests need to be able to work stand alone, and eventually, chained together
 
-    def scroll_right_queue_row(self):
+    def test_scroll_right_queue_row(self):
         home_page = pagemodels.homepage.HomePage(self.driver)
         queue_row = home_page.get_queue_row()
         home_page.row_page_right(queue_row)
 
-    def scroll_left_queue_row(self):
+    def test_scroll_left_queue_row(self):
         """have to scroll right before scroll left is an option"""
         home_page = pagemodels.homepage.HomePage(self.driver)
         queue_row = home_page.get_queue_row()
         home_page.row_page_right(queue_row)
         home_page.row_page_left(queue_row)
+
+    def tearDown(self):
+        pass
+
 
 
 a = HomePageTests()
--- tests/test_loginpage.py
+++ tests/test_loginpage.py
@@ -14,27 +14,34 @@ import pagemodels.loginpage
 # THIS WILL BE THE ONLY PLACE WHERE driver IS DEFINED. EVERY OTHER TEST OBJECT WILL BE PASSED
 # THE driver FROM THIS TEST OBJECT
 # SET YOUR PATH TO YOUR CHROMEDRIVER
-chromedriver_path = secrets.chromedriver_path
-driver = webdriver.Chrome(executable_path=chromedriver_path)
 
 
 class LoginTests(unittest.TestCase):
-    def __init__(self):
-        self.driver = driver
+    def setUp(self):
+        chromedriver_path = secrets.chromedriver_path
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
 
-    def user_login_main(self):
-        login_page = pagemodels.loginpage.LoginPage(driver)
+    def test_user_login_main(self):
+        login_page = pagemodels.loginpage.LoginPage(self.driver)
         login_page.load()
         login_page.user_login(
             secrets.bradleys_email, secrets.bradleys_password
         )
         # print("login was successfull?")
 
-    # def user_login_incorrect_password(self):
-    #     """ """
-    #     pass
+    def test_user_login_incorrect_credentials(self):
+        """ """
+        pass
+
+    def test_user_login_incorrect_email(self):
+        """ """
+        pass
+
+    def tearDown(self):
+        """ """
+        self.driver.quit()
+
 
 
 if __name__ == "__main__":
-    a = LoginTests()
-    a.user_login_main()
+    unittest.main()
